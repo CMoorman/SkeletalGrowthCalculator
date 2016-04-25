2bf4689e4af1c94cd6a494de926da8347ea04c43
Brantlyn Decker <brantlyndecker2@gmail.com>
2016-04-25 16:23:56 -0400

    Testing writes out to file instead of to the console

--- SkeletalGrowthCalculator/src/testing/FelsTest.java
+++ SkeletalGrowthCalculator/src/testing/FelsTest.java
@@ -1,5 +1,8 @@
 package testing;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import main.FELSMethod;
@@ -7,6 +10,7 @@ import statistics.SkeletalEstimation;
 
 public class FelsTest {
 	private static SkeletalEstimation fels = new FELSMethod();
+	private static final String TEST_OUTPUT = "felsTestOutput.txt";
 	public static void test(){
 		List<Patient> patients = PatientValidationDataLoader.loadSampleData();
 		if(patients == null || patients.isEmpty()){
@@ -14,14 +18,27 @@ public class FelsTest {
 			return;
 		}
 		fels.loadData();
+		File outputFile = new File(TEST_OUTPUT);
+		if(outputFile.exists()){
+			outputFile.delete();
+			outputFile = new File(TEST_OUTPUT);
+		}
+		StringBuilder fileContents = new StringBuilder();
 		for(Patient patient : patients){
 			double pascalAge = patient.getPascalSkeletalAge();
 			fels.setAge(patient.getChronologicalAge());
 			fels.setSex(patient.getSex());
 			fels.setInputList(patient.getMeasurements());
 			double generatedAge = fels.performEstimation();
-			System.out.println("The pascal Age for patient " + patient.getPatientID() + " at chronological age " + patient.getChronologicalAge() + " is " + pascalAge + ".");
-			System.out.println("The Age calculated from our program for patient " + patient.getPatientID() + " at chronological age " + patient.getChronologicalAge() + " is " + generatedAge + ".");
+			fileContents.append("The pascal Age for patient " + patient.getPatientID() + " at chronological age " + patient.getChronologicalAge() + " is " + pascalAge + ".\n");
+			fileContents.append("The Age calculated from our program for patient " + patient.getPatientID() + " at chronological age " + patient.getChronologicalAge() + " is " + generatedAge + ".\n");
 		}
+		try {
+			FileWriter fw = new FileWriter(outputFile);
+			fw.write(fileContents.toString());
+			fw.close();
+		} catch (IOException e) {
+		}
+		
 	}
 }
